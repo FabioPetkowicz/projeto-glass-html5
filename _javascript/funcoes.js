121b591b628a95451e6cf1448c13710c45ae6185
Fábio Petkowiz <fopetkowicz@gmail.com>
2020-12-14 23:22:31 -0300

    Meu primeiro projeto em Html5

---
+++ _javascript/funcoes.js
@@ -0,0 +1,7 @@
+/*Funções em Javascript*/
+
+function mudaFoto(foto) {
+    document.getElementById("icone").src = foto;
+}
+
+//***********************************************
---
+++ readme.md
@@ -0,0 +1,3 @@
+- Projeto de Site Google Glass -
+-> Utilizando Html5, CSS3 e JavaScript.
+>>>Proposto pelo site Curso em Vídeo de Gustavo Guanabara.
